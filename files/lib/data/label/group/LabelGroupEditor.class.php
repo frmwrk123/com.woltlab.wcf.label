2061dbe96aeec7fa9b3015ed38f073eb8f9e013a
Alexander Ebert <ebert@woltlab.com>
2012-01-03 02:49:01 +0100

    Cache is now cleared and ACL values removed

--- files/lib/data/label/group/LabelGroupEditor.class.php
+++ files/lib/data/label/group/LabelGroupEditor.class.php
@@ -1,6 +1,9 @@
 <?php
 namespace wcf\data\label\group;
 use wcf\data\DatabaseObjectEditor;
+use wcf\data\IEditableCachedObject;
+use wcf\system\acl\ACLHandler;
+use wcf\system\cache\CacheHandler;
 
 /**
  * Extends the label group object with functions to create, update and delete label groups.
@@ -12,9 +15,29 @@ use wcf\data\DatabaseObjectEditor;
  * @subpackage	data.label.group
  * @category 	Community Framework
  */
-class LabelGroupEditor extends DatabaseObjectEditor {
+class LabelGroupEditor extends DatabaseObjectEditor implements IEditableCachedObject {
 	/**
 	 * @see	wcf\data\DatabaseObjectEditor::$baseClass
 	 */
 	protected static $baseClass = 'wcf\data\label\group\LabelGroup';
+	
+	/**
+	 * @see wcf\data\IEditableObject::deleteAll()
+	 */
+	public static function deleteAll(array $objectIDs = array()) {
+		$count = parent::deleteAll($objectIDs);
+		
+		// remove ACL values
+		$objectTypeID = ACLHandler::getInstance()->getObjectTypeID('com.woltlab.wcf.label');
+		ACLHandler::getInstance()->removeValues($objectIDs, $objectIDs);
+		
+		return $count;
+	}
+	
+	/**
+	 * @see	wcf\data\IEditableCachedObject::resetCache()
+	 */
+	public static function resetCache() {
+		CacheHandler::getInstance()->clear(WCF_DIR.'cache/', 'cache.label.php');
+	}
 }
