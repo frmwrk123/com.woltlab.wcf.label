3a8b30ab704ce5cb687cb1ece3ff68288396e9f5
Alexander Ebert <ebert@woltlab.com>
2012-02-13 15:24:47 +0100

    Removing i18n values once deleting labels
    Fixes #14

--- files/lib/data/label/LabelAction.class.php
+++ files/lib/data/label/LabelAction.class.php
@@ -1,6 +1,9 @@
 <?php
 namespace wcf\data\label;
 use wcf\data\AbstractDatabaseObjectAction;
+use wcf\data\language\item\LanguageItemAction;
+use wcf\system\database\util\PreparedStatementConditionBuilder;
+use wcf\system\WCF;
 
 /**
  * Executes label-related actions.
@@ -32,4 +35,40 @@ class LabelAction extends AbstractDatabaseObjectAction {
 	 * @see	wcf\data\AbstractDatabaseObjectAction::$permissionsUpdate
 	 */
 	protected $permissionsUpdate = array('admin.content.label.canEditLabel');
+	
+	/**
+	 * @see wcf\data\AbstractDatabaseObjectAction::delete()
+	 */
+	public function delete() {
+		$count = parent::delete();
+		
+		if (!empty($this->objects)) {
+			// identify i18n labels
+			$languageVariables = array();
+			foreach ($this->objects as $object) {
+				if (preg_match('~wcf.acp.label.label\d+~', $object->label)) {
+					$languageVariables[] = $object->label;
+				}
+			}
+			
+			// remove language variables
+			if (!empty($languageVariables)) {
+				$conditions = new PreparedStatementConditionBuilder();
+				$conditions->add("languageItem IN (?)", array($languageVariables));
+				
+				$sql = "SELECT	languageItemID
+					FROM	wcf".WCF_N."_language_item
+					".$conditions;
+				$statement = WCF::getDB()->prepareStatement($sql);
+				$statement->execute($conditions->getParameters());
+				$languageItemIDs = array();
+				while ($row = $statement->fetchArray()) {
+					$languageItemIDs[] = $row['languageItemID'];
+				}
+				
+				$objectAction = new LanguageItemAction($languageItemIDs, 'delete');
+				$objectAction->executeAction();
+			}
+		}
+	}
 }
