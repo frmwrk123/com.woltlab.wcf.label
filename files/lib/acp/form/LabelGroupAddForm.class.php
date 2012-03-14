06cfb272e27cac6e4723fb49d2ea020ae75cc52c
Alexander Ebert <ebert@woltlab.com>
2012-03-14 18:33:23 +0100

    Reading object type handler

--- files/lib/acp/form/LabelGroupAddForm.class.php
+++ files/lib/acp/form/LabelGroupAddForm.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace wcf\acp\form;
 use wcf\data\label\group\LabelGroupAction;
+use wcf\data\object\type\ObjectTypeCache;
 use wcf\system\acl\ACLHandler;
 use wcf\system\exception\UserInputException;
 use wcf\system\WCF;
@@ -39,6 +40,18 @@ class LabelGroupAddForm extends ACPForm {
 	public $groupName = '';
 	
 	/**
+	 * list of label object type handlers
+	 * @var	array<wcf\system\label\object\type\ILabelObjectTypeHandler>
+	 */
+	public $labelObjectTypes = array();
+	
+	/**
+	 * list of label object type containers
+	 * @var	array<wcf\system\label\object\type\LabelObjectTypeContainer>
+	 */
+	public $labelObjectTypeContainers = array();
+	
+	/**
 	 * object type id
 	 * @var	integer
 	 */
@@ -49,8 +62,14 @@ class LabelGroupAddForm extends ACPForm {
 	 */
 	public function readParameters() {
 		parent::readParameters();
-	
+		
 		$this->objectTypeID = ACLHandler::getInstance()->getObjectTypeID('com.woltlab.wcf.label');
+		
+		// get label object type handlers
+		$objectTypes = ObjectTypeCache::getInstance()->getObjectTypes('com.woltlab.wcf.label.objectType');
+		foreach ($objectTypes as $objectType) {
+			$this->labelObjectTypes[] = $objectType->getProcessor();
+		}
 	}
 	
 	/**
@@ -60,6 +79,17 @@ class LabelGroupAddForm extends ACPForm {
 		parent::readFormParameters();
 		
 		if (isset($_POST['groupName'])) $this->groupName = StringUtil::trim($_POST['groupName']);
+	}
+	
+	/**
+	 * @see wcf\page\IPage::readData()
+	 */
+	public function readData() {
+		parent::readData();
+		
+		foreach ($this->labelObjectTypes as $labelObjectType) {
+			$this->labelObjectTypeContainers[] = $labelObjectType->getObjects();
+		}
 	}
 	
 	/**
@@ -109,6 +139,7 @@ class LabelGroupAddForm extends ACPForm {
 		WCF::getTPL()->assign(array(
 			'action' => 'add',
 			'groupName' => $this->groupName,
+			'labelObjectTypeContainers' => $this->labelObjectTypeContainers,
 			'objectTypeID' => $this->objectTypeID
 		));
 	}
